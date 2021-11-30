a306e0bf482a319906ff1a649423449b5c7cd816
MwitaBrian <brianmwita13@gmail.com>
2021-11-30 23:56:39 +0300

    Create directory structure and Components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserNameComponent } from './user-name/user-name.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    UserNameComponent,
+    RepositoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

0481ffc59b78deb604075df4635a521826d29dd1
MwitaBrian <brianmwita13@gmail.com>
2021-12-01 02:55:11 +0300

    Create search service

--- src/app/users.ts
+++ src/app/users.ts
@@ -2,7 +2,9 @@ export class Users {
   constructor(
     public login: string,
     public id: number,
-    public avatar_url: string
+    public avatar_url: string,
+    public public_repos: number
+    
   ){}
-  
+
 }

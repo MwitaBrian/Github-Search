3e2e10060ec754b2f24e50b3a5d013db13a8a3d1
MwitaBrian <brianmwita13@gmail.com>
2021-12-01 04:24:11 +0300

    Add search service functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClient } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
--- src/app/search-service/search.service.ts
+++ src/app/search-service/search.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { rejects } from 'assert';
+import { resolve } from 'dns';
+import { Users } from '../users'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  user: Users;
+
+  getUserData(userName:string){
+    interface ApiResponse{
+      login: string,
+      id: number,
+      avatar_url: string,
+      public_repos: number
+    }
+    let promise = new Promise<void>((resolve, reject)=>{
+      this.http.get<ApiResponse>(`https://api.github.com/users/${userName}`).toPromise().then(response=>{
+        this.user.login = response.login;
+        this.user.id = response.id;
+        this.user.avatar_url = response.avatar_url;
+        this.user.public_repos = response.public_repos;
+
+        resolve()
+    },
+    error=>{
+      this.user.login = 'User not found'
+      console.log("An error occured")
+      reject(error)
+    })
+    })
+    return promise;
+  }
+
+  constructor( private http: HttpClient) { }
 }
